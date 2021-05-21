3282427a56a017e6e30095d81f489a2c00c56f2c
Kiran J. Holla <kjholla@gmail.com>
2021-05-21 15:10:18 +0530

    Making the district generic

--- src/check-cowin.js
+++ src/check-cowin.js
@@ -12,21 +12,23 @@ function errorHandler(e) {
   console.log(e);
 }
 
+function checkForDate(district, date) {
+  return axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district}&date=${date}`, {
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-US,en;q=0.5',
+      Origin: 'https://www.cowin.gov.in/',
+      Referer: 'https://www.cowin.gov.in/',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
+    }
+  });
+}
+
 export function checkCowin() {
   const todayDate = new Date();
   const todayStr = `${todayDate.getDate()}-${leftPad(todayDate.getMonth() + 1, 2, 0, '0')}-${todayDate.getFullYear()}`;
 
-  axios
-    .get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=266&date=${todayStr}`, {
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.5',
-        Origin: 'https://www.cowin.gov.in/',
-        Referer: 'https://www.cowin.gov.in/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
-      }
-    })
-    .then(responseHandler)
-    .catch(errorHandler);
+  const district = 266;
+  checkForDate(district, todayStr).then(responseHandler).catch(errorHandler);
 }
