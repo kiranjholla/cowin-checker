8555db801d7215ad54128e2ba6e5d2f147595a9b
Kiran J. Holla <kjholla@gmail.com>
2021-05-21 20:28:19 +0530

    Fix the isNumeric function to catch strings that start with a number

--- src/utils/num-utils.js
+++ src/utils/num-utils.js
@@ -4,5 +4,6 @@ import isNumber from 'lodash/isNumber.js';
 
 // https://github.com/lodash/lodash/issues/1148#issuecomment-603841139
 export function isNumeric(value) {
-  return isNumber(value) || (!isEmpty(value) && !isNaN(parseFloat(value)));
+  // return /^[\+\-]?[0123456789]+(?:(?:e[0123456789]+)|(?:\.[0123456789]+))?$/.test(value);
+  return !isNaN(Number(`${value}`))
 }
